e855522dea1635945aa5dab550d0653dc1f3c119
ursarah <sarahurcezino@gmail.com>
2023-01-09 09:57:29 -0300

    novos exercicios

---
+++ script.js
@@ -0,0 +1,3 @@
+function digita(){ 
+    alert(`Olá meu nome é ${document.getElementById('nome').value} ${document.getElementById('sobrenome').value} e tenho ${document.getElementById('idade').value}`)
+}
